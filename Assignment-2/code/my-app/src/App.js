e441bbc96e21a1975c368a1f7444d05126e041cb
NagaarjunanL <nagaarjunan1998@gmail.com>
2022-02-05 12:00:24 +0530

    Assignment2_submission

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -7,8 +7,25 @@ function App(props) {
  const {employee}=props;
     
 return(
-  <div className="container">
+  <div className="container App">
   {/* Start your React code here */}
+  <div className="profile-image-container">
+    <img className="profile" src ={employee.prodileImg} width ="100" height="100" alt ="Profile image"/>
+  </div>
+  <div className="title">
+    <h1>{employee.name}</h1>
+  </div>
+  <div className="profile_info">
+    <label>Location</label>
+    <strong>{employee.location}</strong>
+  </div>
+  <div className="profile_info">
+  <label>Blood group</label>
+  <strong>{employee.bloodGroup}</strong>
+  </div>\<div className="profile_info">
+  <label>Age</label>
+  <strong>{employee.age}</strong>
+    </div>
 </div>
 )
 }
